0aca3498c08e2c38991e1d05babfbc46897e4c46
lazzari.ale@gmail.com <lazzari.ale@gmail.com@bd8f525f-ddb9-16a4-faaa-1f9a8cd287b0>
2014-01-25 21:57:10 +0000

    release version 1.4.5
    git-svn-id: http://android-money-manager-ex.googlecode.com/svn/trunk/android-money-manager-ex@488 bd8f525f-ddb9-16a4-faaa-1f9a8cd287b0

--- src/com/money/manager/ex/about/AboutFragment.java
+++ src/com/money/manager/ex/about/AboutFragment.java
@@ -2,8 +2,10 @@ package com.money.manager.ex.about;
 
 import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,7 +13,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.money.manager.ex.DonateActivity;
@@ -19,6 +23,31 @@ import com.money.manager.ex.R;
 
 public class AboutFragment extends SherlockFragment {
 	private static final String LOGCAT = AboutFragment.class.getSimpleName();
+	// implement a class to manage the opening of several url
+	private class OnClickListenerUrl implements OnClickListener {
+		private String mUrl;
+		
+		@Override
+		public void onClick(View v) {
+			if (TextUtils.isEmpty(getUrl()))
+				return;
+			try {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getUrl()));
+				startActivity(intent);
+			} catch (Exception e) {
+				Toast.makeText(getSherlockActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
+			}
+		}
+
+		public String getUrl() {
+			return mUrl;
+		}
+
+		public void setUrl(String mUrl) {
+			this.mUrl = mUrl;
+		}
+		
+	}	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.about_activity, container, false);
@@ -27,19 +56,55 @@ public class AboutFragment extends SherlockFragment {
 		// take a object into layout
 		String text;
 		TextView txtFeedback = (TextView)view.findViewById(R.id.textViewLinkFeedback);
-		text = "<a href=\"mailto:android.money.manager.ex@gmail.com\">" + (String) txtFeedback.getText() + "</a>";
+		text = "<u>" + (String) txtFeedback.getText() + "</u>";
 		txtFeedback.setText(Html.fromHtml(text));
-		txtFeedback.setMovementMethod(LinkMovementMethod.getInstance());
+		txtFeedback.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_SEND);
+				intent.setType("message/rfc822");
+				intent.putExtra(Intent.EXTRA_EMAIL  , new String[]{"android.money.manager.ex@gmail.com"});
+				intent.putExtra(Intent.EXTRA_SUBJECT, "MoneyManagerEx for Android: Feedback");
+				try {
+				    startActivity(Intent.createChooser(intent, "Send mail..."));
+				} catch (Exception e) {
+				    Toast.makeText(getSherlockActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+		// rate application
+		TextView txtRate = (TextView)view.findViewById(R.id.textViewLinkRate);
+		text = "<u>" + (String) txtRate.getText() + "</u>";
+		txtRate.setText(Html.fromHtml(text));
+		txtRate.setMovementMethod(LinkMovementMethod.getInstance());
+		OnClickListenerUrl clickListenerRate = new OnClickListenerUrl();
+		clickListenerRate.setUrl("http://play.google.com/store/apps/details?id=com.money.manager.ex");
+		txtRate.setOnClickListener(clickListenerRate);
 		// report set link
 		TextView txtReport = (TextView)view.findViewById(R.id.textViewLinkProblem);
-		text = "<a href=\"http://code.google.com/p/android-money-manager-ex/issues/entry\">" + (String) txtReport.getText() + "</a>";
+		text = "<u>" + (String) txtReport.getText() + "</u>";
 		txtReport.setText(Html.fromHtml(text));
 		txtReport.setMovementMethod(LinkMovementMethod.getInstance());
+		OnClickListenerUrl clickListenerFeedback = new OnClickListenerUrl();
+		clickListenerFeedback.setUrl("http://code.google.com/p/android-money-manager-ex/issues/list");
+		txtReport.setOnClickListener(clickListenerFeedback);
+		// image view google plus
+		OnClickListenerUrl clickListenerGooglePlus = new OnClickListenerUrl();
+		clickListenerGooglePlus.setUrl("http://goo.gl/R693Ih");
+		ImageView imageViewGooglePlus = (ImageView)view.findViewById(R.id.imageViewGooglePlus);
+		imageViewGooglePlus.setOnClickListener(clickListenerGooglePlus);
+		// image view twitter
+		OnClickListenerUrl clickListenerTwitter = new OnClickListenerUrl();
+		clickListenerTwitter.setUrl("https://twitter.com/MMEXForAndroid");
+		ImageView imageViewTwitter = (ImageView)view.findViewById(R.id.imageViewTwitter);
+		imageViewTwitter.setOnClickListener(clickListenerTwitter);
 		// GPLv2 license
 		TextView txtLicense = (TextView)view.findViewById(R.id.textViewLicense);
-		text = "<a href=\"http://www.gnu.org/licenses/old-licenses/gpl-2.0.html\">" + (String) txtLicense.getText() + "</a>";
+		text = "<u>" + (String) txtLicense.getText() + "</u>";
 		txtLicense.setText(Html.fromHtml(text));
-		txtLicense.setMovementMethod(LinkMovementMethod.getInstance());
+		OnClickListenerUrl clickListenerLicense = new OnClickListenerUrl();
+		clickListenerLicense.setUrl("http://www.gnu.org/licenses/old-licenses/gpl-2.0.html");
+		txtLicense.setOnClickListener(clickListenerLicense);
 		// Version application
 		TextView txtVersion = (TextView)view.findViewById(R.id.textViewVersion);
 		TextView txtBuild = (TextView)view.findViewById(R.id.textViewBuild);
