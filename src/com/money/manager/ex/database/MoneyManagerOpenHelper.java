7709a7b1772b780363be06754a03fa6ea89f91c5
AlessandroLazzari <lazzari.ale@gmail.com>
2014-12-03 19:34:23 +0100

    set journal_mode of sqlite off. This change allows not to create the journal

--- src/com/money/manager/ex/database/MoneyManagerOpenHelper.java
+++ src/com/money/manager/ex/database/MoneyManagerOpenHelper.java
@@ -1,5 +1,5 @@
-/*******************************************************************************
- * Copyright (C) 2012 The Android Money Manager Ex Project
+/*
+ * Copyright (C) 2012-2014 Alessandro Lazzari
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- ******************************************************************************/
+ */
 package com.money.manager.ex.database;
 
 import android.content.Context;
@@ -68,6 +68,7 @@ public class MoneyManagerOpenHelper extends SQLiteOpenHelper {
     public void onConfigure(SQLiteDatabase db) {
         super.onConfigure(db);
         Log.v(LOGCAT, "event onConfigure( )");
+        db.rawQuery("PRAGMA journal_mode=OFF", null).close();
     }
 
     @Override
