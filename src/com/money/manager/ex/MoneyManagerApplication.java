4a5d3696b60191d0dfa96e3b704846e2b6ad45ce
Alessandro Lazzari <lazzari.ale@gmail.com>
2014-12-27 16:47:07 +0100

    Updating also the default database in the Preferences

--- src/com/money/manager/ex/MoneyManagerApplication.java
+++ src/com/money/manager/ex/MoneyManagerApplication.java
@@ -1,20 +1,20 @@
-/*******************************************************************************
- * Copyright (C) 2012 The Android Money Manager Ex Project
- * 
+/*
+ * Copyright (C) 2012-2014 Alessandro Lazzari
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- ******************************************************************************/
+ */
 package com.money.manager.ex;
 
 import android.annotation.SuppressLint;
@@ -143,7 +143,8 @@ public class MoneyManagerApplication extends Application {
 		if (f.getAbsoluteFile().exists()) {
 			return dbFile;
 		} else {
-			return defaultPath;
+            MoneyManagerApplication.setDatabasePath(context, defaultPath);
+            return defaultPath;
 		}
 	}
 	
