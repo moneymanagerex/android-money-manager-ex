007115ff9b02f4337a377420329a110c0beda1a1
AlessandroLazzari <lazzari.ale@gmail.com>
2014-12-31 10:31:40 +0100

    Fix headerTextViewStyle style

--- src/com/money/manager/ex/fragment/BaseListFragment.java
+++ src/com/money/manager/ex/fragment/BaseListFragment.java
@@ -18,13 +18,10 @@
 package com.money.manager.ex.fragment;
 
 import android.animation.LayoutTransition;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ListFragment;
-import android.support.v4.widget.SearchViewCompat;
-import android.support.v4.widget.SearchViewCompat.OnQueryTextListenerCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
@@ -32,13 +29,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 
 import com.money.manager.ex.MainActivity;
 import com.money.manager.ex.R;
 import com.money.manager.ex.core.Core;
+import com.money.manager.ex.core.SearchViewFormatter;
 import com.money.manager.ex.preferences.PreferencesConstant;
 
 public abstract class BaseListFragment extends ListFragment {
@@ -50,6 +45,8 @@ public abstract class BaseListFragment extends ListFragment {
     private boolean mMenuItemSearchIconified = true;
     // flag for tips wildcard
     private boolean isShowTipsWildcard = false;
+    // hint searchview
+    private String mSearchHint = null;
 
     public abstract String getSubTitle();
 
@@ -111,36 +108,36 @@ public abstract class BaseListFragment extends ListFragment {
         if (isShowMenuItemSearch() && getActivity() != null && getActivity() instanceof ActionBarActivity) {
             // Place an action bar item for searching.
             final MenuItem itemSearch = menu.add(0, R.id.menu_query_mode, 1000, R.string.search);
-            itemSearch.setIcon(new Core(getActivity()).resolveIdAttribute(R.attr.ic_action_search));
-            itemSearch.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
 
+            itemSearch.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
             ActionBarActivity activity = (ActionBarActivity) getActivity();
 
-            View searchView = SearchViewCompat.newSearchView(activity.getSupportActionBar().getThemedContext());
+            SearchView searchView = new SearchView(getActivity());
             if (searchView != null) {
-                SearchViewCompat.setOnQueryTextListener(searchView, new OnQueryTextListenerCompat() {
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                     @Override
-                    public boolean onQueryTextChange(String newText) {
-                        return BaseListFragment.this.onPreQueryTextChange(newText);
-                    }
-                });
-                SearchViewCompat.setIconified(searchView, isMenuItemSearchIconified());
-                itemSearch.setActionView(searchView);
-            } else {
-                SearchView actionSearchView = new SearchView(activity.getSupportActionBar().getThemedContext());
-                actionSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-                    @Override
-                    public boolean onQueryTextSubmit(String query) {
+                    public boolean onQueryTextSubmit(String s) {
                         return false;
                     }
 
                     @Override
-                    public boolean onQueryTextChange(String newText) {
-                        return BaseListFragment.this.onPreQueryTextChange(newText);
+                    public boolean onQueryTextChange(String s) {
+                        return BaseListFragment.this.onPreQueryTextChange(s);
                     }
                 });
-                itemSearch.setActionView(actionSearchView);
+                searchView.setIconified(isMenuItemSearchIconified());
+                itemSearch.setActionView(searchView);
+
+                SearchViewFormatter formatter = new SearchViewFormatter();
+
+                formatter.setSearchIconResource(R.drawable.ic_action_search_dark, true, true);
+                formatter.setSearchCloseIconResource(R.drawable.ic_action_content_clear_dark);
+                formatter.setSearchTextColorResource(R.color.abc_primary_text_material_dark);
+                formatter.setSearchHintColorResource(R.color.hint_foreground_material_dark);
+                setSearchHint("Hint");
+                formatter.setSearchHintText(getSearchHint());
+
+                formatter.format(searchView);
             }
         }
     }
@@ -154,46 +151,8 @@ public abstract class BaseListFragment extends ListFragment {
                 // set result and exit
                 this.setResultAndFinish();
                 return true; // consumed here
-            case R.id.menu_query_mode:
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
-                    onMenuItemSearchClick(item);
-                return true; // consumed here
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    protected void onMenuItemSearchClick(MenuItem item) {
-        if (getActivity() != null && getActivity() instanceof ActionBarActivity) {
-            ActionBarActivity activity = (ActionBarActivity) getActivity();
-            View searchView = activity.getSupportActionBar().getCustomView();
-            final EditText edtSearch = (EditText) searchView.findViewById(R.id.editTextSearchView);
-            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-            // se in visualizzazione prendo l'edittext
-            if (mDisplayShowCustomEnabled == false) {
-                // rendo visibile l'edittext di ricerca
-                edtSearch.setText("");
-                edtSearch.requestFocus();
-                // rendo visibile la keyboard
-                imm.showSoftInput(edtSearch, 0);
-                item.setActionView(searchView);
-                // aggiorno lo stato
-                mDisplayShowCustomEnabled = true;
-            } else {
-                // controllo se ho del testo lo pulisco
-                if (TextUtils.isEmpty(String.valueOf(edtSearch.getText()))) {
-                    // nascondo la keyboard
-                    imm.hideSoftInputFromWindow(edtSearch.getWindowToken(), 0);
-                    // tolgo la searchview
-                    item.setActionView(null);
-                    // aggiorno lo stato
-                    mDisplayShowCustomEnabled = false;
-                } else {
-                    // annullo il testo
-                    edtSearch.setText(null);
-                    onPreQueryTextChange("");
-                }
-            }
         }
     }
 
@@ -235,9 +194,7 @@ public abstract class BaseListFragment extends ListFragment {
     }
 
     public void setResultAndFinish() {
-        // chiamo l'impostazione dei dati e chiudo l'activity
         this.setResult();
-        // chiudo l'activity dove sono collegato
         getActivity().finish();
     }
 
@@ -247,5 +204,13 @@ public abstract class BaseListFragment extends ListFragment {
 
     public void setMenuItemSearchIconified(boolean mMenuItemSearchIconified) {
         this.mMenuItemSearchIconified = mMenuItemSearchIconified;
+    }
+
+    public String getSearchHint() {
+        return mSearchHint;
+    }
+
+    public void setSearchHint(String mSearchHint) {
+        this.mSearchHint = mSearchHint;
     }
 }
