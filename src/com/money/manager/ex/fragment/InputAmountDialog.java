cc7b83eca1b37fa53d0eb1b2009483c5478b2517
AlessandroLazzari <lazzari.ale@gmail.com>
2015-02-09 22:25:53 +0100

    add calculator on InputDialogAmout

--- src/com/money/manager/ex/fragment/InputAmountDialog.java
+++ src/com/money/manager/ex/fragment/InputAmountDialog.java
@@ -23,6 +23,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -38,6 +39,9 @@ import com.money.manager.ex.core.Core;
 import com.money.manager.ex.utils.CurrencyUtils;
 import com.money.manager.ex.utils.MathUtils;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class InputAmountDialog extends DialogFragment {
@@ -46,17 +50,20 @@ public class InputAmountDialog extends DialogFragment {
     private static final String KEY_ID_VIEW = "InputAmountDialog:Id";
     private static final String KEY_AMOUNT = "InputAmountDialog:Amount";
     private static final String KEY_CURRENCY_ID = "InputAmountDialog:CurrencyId";
+    private static final String KEY_EXPRESSION = "InputAmountDialog:Expression";
     private static final String COMMA_DECIMAL = ".";
 
     // arrays id keynum button
     private int[] idButtonKeyNum = {R.id.buttonKeyNum0, R.id.buttonKeyNum1, R.id.buttonKeyNum2, R.id.buttonKeyNum3,
             R.id.buttonKeyNum4, R.id.buttonKeyNum5, R.id.buttonKeyNum6, R.id.buttonKeyNum7, R.id.buttonKeyNum8,
-            R.id.buttonKeyNum9, R.id.buttonKeyNumDecimal};
+            R.id.buttonKeyNum9, R.id.buttonKeyNumDecimal, R.id.buttonKeyAdd, R.id.buttonKeyDiv,
+            R.id.buttonKeyLess, R.id.buttonKeyMultiplication, R.id.buttonKeyLeftParenthesis, R.id.buttonKeyRightParenthesis};
 
     private int mIdView;
     private String mAmount = Constants.EMPTY_STRING;
-    private Integer mCurrencyId;
-    private TextView txtAmount;
+    private String mExpression = null;
+    private Integer mCurrencyId, mDefaultColor;
+    private TextView txtAmount, txtExpression;
     private ImageButton imgDelete;
 
     public static InputAmountDialog getInstance(int id, Double amount) {
@@ -91,6 +98,7 @@ public class InputAmountDialog extends DialogFragment {
                 } else {
                     mAmount = Double.toString(amount);
                 }
+                mExpression = mAmount;
             }
         } else {
             if (savedInstanceState.containsKey(KEY_AMOUNT))
@@ -99,6 +107,8 @@ public class InputAmountDialog extends DialogFragment {
                 mCurrencyId = savedInstanceState.getInt(KEY_CURRENCY_ID);
             if (savedInstanceState.containsKey(KEY_ID_VIEW))
                 mIdView = savedInstanceState.getInt(KEY_ID_VIEW);
+            if (savedInstanceState.containsKey(KEY_EXPRESSION))
+                mExpression = savedInstanceState.getString(KEY_EXPRESSION);
         }
     }
 
@@ -111,7 +121,7 @@ public class InputAmountDialog extends DialogFragment {
         OnClickListener clickListener = new OnClickListener() {
             @Override
             public void onClick(View v) {
-                String parseAmountTry = mAmount;
+                /*String parseAmountTry = mAmount;
                 parseAmountTry += ((Button) v).getText();
                 // check if amount string is empty
                 if (TextUtils.isEmpty(parseAmountTry))
@@ -122,7 +132,8 @@ public class InputAmountDialog extends DialogFragment {
                     // change amount
                     mAmount = parseAmountTry;
                     refreshAmount();
-                }
+                }*/
+                txtExpression.setText(txtExpression.getText().toString().concat(((Button) v).getText().toString()));
             }
         };
         // reference button click listener
@@ -130,24 +141,41 @@ public class InputAmountDialog extends DialogFragment {
             Button button = (Button) view.findViewById(id);
             button.setOnClickListener(clickListener);
         }
+        // button equals
+        Button buttonKeyEquals = (Button) view.findViewById(R.id.buttonKeyEqual);
+        buttonKeyEquals.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                evalExprension();
+            }
+        });
         // image button delete
         imgDelete = (ImageButton) view.findViewById(R.id.imageButtonCancel);
         imgDelete.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                if (!TextUtils.isEmpty(mAmount)) {
+                /*if (!TextUtils.isEmpty(mAmount)) {
                     if (mAmount.endsWith(COMMA_DECIMAL)) {
                         mAmount = mAmount.substring(0, mAmount.length() - 2);
                     } else {
                         mAmount = mAmount.substring(0, mAmount.length() - 1);
                     }
                 }
-                refreshAmount();
+                refreshAmount();*/
+                String exp = txtExpression.getText().toString();
+                if (exp.length() > 0) {
+                    exp = exp.substring(0, exp.length() - 1);
+                    txtExpression.setText(exp);
+                }
             }
         });
         // reference TextView amount
         txtAmount = (TextView) view.findViewById(R.id.textViewAmount);
+        mDefaultColor = txtAmount.getCurrentTextColor();
+        txtExpression = (TextView) view.findViewById(R.id.textViewExpression);
+        if (!TextUtils.isEmpty(mExpression))
+            txtExpression.setText(mExpression);
 
         MaterialDialog.Builder builder = new MaterialDialog.Builder(getActivity());
         builder.customView(view, false);
@@ -155,20 +183,24 @@ public class InputAmountDialog extends DialogFragment {
         builder.callback(new MaterialDialog.ButtonCallback() {
             @Override
             public void onPositive(MaterialDialog dialog) {
+                if (!evalExprension())
+                    return;
+
                 if (TextUtils.isEmpty(mAmount))
                     mAmount = Double.toString(0);
                 // check if is double
                 if (NumberUtils.isNumber(mAmount)) {
                     ((InputAmountDialogListener) getActivity()).onFinishedInputAmountDialog(mIdView, MathUtils.Round(Double.parseDouble(mAmount), 2));
-                    dismiss();
+                    dialog.dismiss();
                 }
             }
 
             @Override
             public void onNegative(MaterialDialog dialog) {
-                dismiss();
+                dialog.dismiss();
             }
         });
+        builder.autoDismiss(false);
         builder.negativeText(android.R.string.cancel);
         builder.positiveText(android.R.string.ok);
         builder.theme(core.getThemeApplication() == R.style.Theme_Money_Manager ? Theme.DARK : Theme.LIGHT);
@@ -191,6 +223,7 @@ public class InputAmountDialog extends DialogFragment {
         if (mCurrencyId != null)
             savedInstanceState.putInt(KEY_CURRENCY_ID, mCurrencyId);
         savedInstanceState.putInt(KEY_ID_VIEW, mIdView);
+        savedInstanceState.putString(KEY_EXPRESSION, txtExpression.getText().toString());
     }
 
     public void refreshAmount() {
@@ -210,6 +243,26 @@ public class InputAmountDialog extends DialogFragment {
                 txtAmount.setText(currencyUtils.getCurrencyFormatted(mCurrencyId, fAmount));
             }
         }
+    }
+
+    public boolean evalExprension() {
+        String exp = txtExpression.getText().toString();
+        if (exp.length() > 0) {
+            try {
+                Expression e = new ExpressionBuilder(exp).build();
+                double result = e.evaluate();
+                mAmount = Double.toString(result);
+                refreshAmount();
+                txtAmount.setTextColor(mDefaultColor);
+                return true;
+            } catch (Exception e) {
+                Log.e(LOGCAT, e.getMessage());
+                txtAmount.setText(R.string.invalid_expression);
+                txtAmount.setTextColor(getResources().getColor(R.color.material_red_700));
+                return false;
+            }
+        }
+        return true;
     }
 
     public interface InputAmountDialogListener {
