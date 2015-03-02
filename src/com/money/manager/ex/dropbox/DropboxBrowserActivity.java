6b63c3f2dfe9411eca5800bfafa407733b4e0cbe
AlessandroLazzari <lazzari.ale@gmail.com>
2015-03-02 22:28:18 +0100

    fixed leaked memory with change context to getApplicationContext()

--- src/com/money/manager/ex/dropbox/DropboxBrowserActivity.java
+++ src/com/money/manager/ex/dropbox/DropboxBrowserActivity.java
@@ -49,13 +49,160 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DropboxBrowserActivity extends BaseFragmentActivity {
-    ublic
-    static final String INTENT_DROBPOXFILE_PATH = "DropboxBrowserActivity:DropboxFile";
+    public static class DropboxBrowserFragment extends BaseListFragment {
+        //Define EntryAdapter
+        private class EntryAdapter extends ArrayAdapter<Entry> {
+            private int mLayoutId;
+            private LayoutInflater mInflater;
 
+            public EntryAdapter(Context context, int resource) {
+                this(context, resource, new ArrayList<Entry>());
+            }
+
+            public EntryAdapter(Context context, int resource, List<Entry> objects) {
+                super(context, resource, objects);
+                mLayoutId = resource;
+                mInflater = LayoutInflater.from(context);
+            }
+
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+                    Entry entry = getItem(position);
+                    if (entry != null) {
+                        convertView = mInflater.inflate(mLayoutId, parent, false);
+                        CheckedTextView text1 = (CheckedTextView)convertView.findViewById(android.R.id.text1);
+                        TextView text2 = (TextView)convertView.findViewById(android.R.id.text2);
+                        text1.setText(entry.path);
+                        text2.setText(getString(R.string.last_modified) + ": " + new SimpleDateFormat().format(RESTUtility.parseDate(entry.modified)));
+                    }
+                }
+                //checked item
+                CheckedTextView text1 = (CheckedTextView)convertView.findViewById(android.R.id.text1);
+                text1.setChecked(getListView().getCheckedItemPosition() == position);
+
+                return convertView;
+            }
+
+        }
+
+        DropboxHelper mHelper;
+        EntryAdapter mAdapter;
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            // take a instance
+            mHelper = DropboxHelper.getInstance(getActivity());
+            mAdapter = new EntryAdapter(getActivity(), R.layout.simple_list_item_multiple_choice_2);
+            // set adapter
+            setListAdapter(mAdapter);
+
+            setEmptyText(getString(R.string.dropbox_empty_folder));
+
+            registerForContextMenu(getListView());
+
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            setListShown(false);
+
+            //check item
+            getListView().setOnItemClickListener(new OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    CheckedTextView checkedTextView = (CheckedTextView)view.findViewById(android.R.id.text1);
+                    checkedTextView.toggle();
+                    getListView().setItemChecked(position, checkedTextView.isChecked());
+                    ((EntryAdapter)getListAdapter()).setNotifyOnChange(true);
+                }
+            });
+
+            //set option menu
+            setHasOptionsMenu(true);
+
+            //take a entries
+            if (mAdapter.getCount() <= 0)
+                refreshEntries();
+        }
+
+        public boolean onKeyUp(int keyCode, KeyEvent event) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) setResultAndFinish();
+            return false;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            inflater.inflate(R.menu.menu_dropbox_browser_activity, menu);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    setResultAndFinish();
+                    return true;
+                case R.id.menu_refresh:
+                    refreshEntries();
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+
+        public void setResultAndFinish() {
+            Intent result = new Intent();
+            if (getListView().getCheckedItemPosition() == ListView.INVALID_POSITION) {
+                result.putExtra(INTENT_DROBPOXFILE_PATH, (String)null);
+            } else {
+                // set result and exit
+                result.putExtra(INTENT_DROBPOXFILE_PATH, mAdapter.getItem(getListView().getCheckedItemPosition()).path);
+            }
+            getActivity().setResult(RESULT_OK, result);
+            // exit
+            getActivity().finish();
+        }
+
+        private void refreshEntries() {
+            mHelper.getEntries(new OnGetEntries() {
+
+                public void onStarting() {
+                    setListShown(false);
+                    mAdapter.clear();
+                    mAdapter.setNotifyOnChange(true);
+                }
+
+                public void onFinished(List<Entry> result) {
+                    if (isVisible()) {
+                        if (result != null) {
+                            for(int i = 0; i < result.size(); i ++) {
+                                if (result.get(i).path.toLowerCase().endsWith(".mmb")) {
+                                    mAdapter.add(result.get(i));
+                                    //check if file is same pass from intent
+                                    if (getListView().getCheckedItemPosition() == ListView.INVALID_POSITION && result.get(i).path.equals(mDropboxFile)) {
+                                        getListView().setItemChecked(mAdapter.getCount() - 1, true);
+                                    }
+                                }
+                            }
+                        }
+                        mAdapter.setNotifyOnChange(true);
+                        setListShown(true);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public String getSubTitle() {
+            return null;
+        }
+    }
+
+    public static final String INTENT_DROBPOXFILE_PATH = "DropboxBrowserActivity:DropboxFile";
     private static String mDropboxFile = null;
-    p
-            Override
 
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -73,166 +220,14 @@ public class DropboxBrowserActivity extends BaseFragmentActivity {
         }
     }
 
-    @
-            Override
+    @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            DropboxBrowserFragment fragment = (DropboxBrowserFragment) getSupportFragmentManager().findFragmentByTag(DropboxBrowserFragment.class.getSimpleName());
+            DropboxBrowserFragment fragment = (DropboxBrowserFragment)getSupportFragmentManager().findFragmentByTag(DropboxBrowserFragment.class.getSimpleName());
             if (fragment != null) {
                 return fragment.onKeyUp(keyCode, event);
             }
         }
         return super.onKeyUp(keyCode, event);
-    }
-}
-@
-
-public static class DropboxBrowserFragment extends BaseListFragment {
-    DropboxHelper mHelper;
-    EntryAdapter mAdapter;
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        // take a instance
-        mHelper = DropboxHelper.getInstance(getActivity());
-        mAdapter = new EntryAdapter(getActivity(), R.layout.simple_list_item_multiple_choice_2);
-        // set adapter
-        setListAdapter(mAdapter);
-
-        setEmptyText(getString(R.string.dropbox_empty_folder));
-
-        registerForContextMenu(getListView());
-
-        getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        setListShown(false);
-
-        //check item
-        getListView().setOnItemClickListener(new OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                CheckedTextView checkedTextView = (CheckedTextView) view.findViewById(android.R.id.text1);
-                checkedTextView.toggle();
-                getListView().setItemChecked(position, checkedTextView.isChecked());
-                ((EntryAdapter) getListAdapter()).setNotifyOnChange(true);
-            }
-        });
-
-        //set option menu
-        setHasOptionsMenu(true);
-
-        //take a entries
-        if (mAdapter.getCount() <= 0)
-            refreshEntries();
-    }
-
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) setResultAndFinish();
-        return false;
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_dropbox_browser_activity, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                setResultAndFinish();
-                return true;
-            case R.id.menu_refresh:
-                refreshEntries();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    public void setResultAndFinish() {
-        Intent result = new Intent();
-        if (getListView().getCheckedItemPosition() == ListView.INVALID_POSITION) {
-            result.putExtra(INTENT_DROBPOXFILE_PATH, (String) null);
-        } else {
-            // set result and exit
-            result.putExtra(INTENT_DROBPOXFILE_PATH, mAdapter.getItem(getListView().getCheckedItemPosition()).path);
-        }
-        getActivity().setResult(RESULT_OK, result);
-        // exit
-        getActivity().finish();
-    }
-
-    private void refreshEntries() {
-        mHelper.getEntries(new OnGetEntries() {
-
-            public void onStarting() {
-                setListShown(false);
-                mAdapter.clear();
-                mAdapter.setNotifyOnChange(true);
-            }
-
-            public void onFinished(List<Entry> result) {
-                if (isVisible()) {
-                    if (result != null) {
-                        for (int i = 0; i < result.size(); i++) {
-                            if (result.get(i).path.toLowerCase().endsWith(".mmb")) {
-                                mAdapter.add(result.get(i));
-                                //check if file is same pass from intent
-                                if (getListView().getCheckedItemPosition() == ListView.INVALID_POSITION && result.get(i).path.equals(mDropboxFile)) {
-                                    getListView().setItemChecked(mAdapter.getCount() - 1, true);
-                                }
-                            }
-                        }
-                    }
-                    mAdapter.setNotifyOnChange(true);
-                    setListShown(true);
-                }
-            }
-        });
-    }
-
-    @Override
-    public String getSubTitle() {
-        return null;
-    }
-
-    //Define EntryAdapter
-    private class EntryAdapter extends ArrayAdapter<Entry> {
-        private int mLayoutId;
-        private LayoutInflater mInflater;
-
-        public EntryAdapter(Context context, int resource) {
-            this(context, resource, new ArrayList<Entry>());
-        }
-
-        public EntryAdapter(Context context, int resource, List<Entry> objects) {
-            super(context, resource, objects);
-            mLayoutId = resource;
-            mInflater = LayoutInflater.from(context);
-        }
-
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-                Entry entry = getItem(position);
-                if (entry != null) {
-                    convertView = mInflater.inflate(mLayoutId, parent, false);
-                    CheckedTextView text1 = (CheckedTextView) convertView.findViewById(android.R.id.text1);
-                    TextView text2 = (TextView) convertView.findViewById(android.R.id.text2);
-                    text1.setText(entry.path);
-                    text2.setText(getString(R.string.last_modified) + ": " + new SimpleDateFormat().format(RESTUtility.parseDate(entry.modified)));
-                }
-            }
-            //checked item
-            CheckedTextView text1 = (CheckedTextView) convertView.findViewById(android.R.id.text1);
-            text1.setChecked(getListView().getCheckedItemPosition() == position);
-
-            return convertView;
-        }
-
     }
 }
