e06652eb1e4cedcdff8fea49de505275cdc5979a
Alen Siljak <alen.siljak@gmail.com>
2015-03-17 21:41:56 +0100

    Opening tutorial activity.

---
+++ src/com/money/manager/ex/adapter/TutorialPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.money.manager.ex.adapter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.money.manager.ex.fragment.TutorialPage1Fragment;
+
+/**
+ * Created by Alen on 17/03/2015.
+ */
+public class TutorialPagerAdapter extends FragmentStatePagerAdapter {
+    public TutorialPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = new TutorialPage1Fragment();
+        Bundle args = new Bundle();
+        // Our object is just an integer :-P
+        args.putInt(TutorialPage1Fragment.ARG_OBJECT, i + 1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 100;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "OBJECT " + (position + 1);
+    }
+}
