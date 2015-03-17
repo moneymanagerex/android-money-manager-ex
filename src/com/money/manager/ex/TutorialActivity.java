67bd98f59ddd997e7dab070344957f2bd913cae3
Alen Siljak <alen.siljak@gmail.com>
2015-03-17 11:22:35 +0100

    Adding a ViewPager activity for Tutorial.

---
+++ src/com/money/manager/ex/TutorialActivity.java
@@ -0,0 +1,50 @@
+package com.money.manager.ex;
+
+import com.money.manager.ex.fragment.BaseFragmentActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ *
+ * See: http://developer.android.com/training/implementing-navigation/lateral.html
+ */
+public class TutorialActivity extends BaseFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+//        mDemoCollectionPagerAdapter =
+//                new DemoCollectionPagerAdapter(
+//                        getSupportFragmentManager());
+//        mViewPager = (ViewPager) findViewById(R.id.pager);
+//        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tutorial, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
