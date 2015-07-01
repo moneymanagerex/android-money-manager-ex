f9cd7018146b939805e8cd0ca07917470438d2a0
Alen Siljak <alen.siljak@yahoo.com>
2015-07-01 11:37:36 +0200

    handling an exception when closing a dialog

--- src/com/money/manager/ex/utils/DialogUtils.java
+++ src/com/money/manager/ex/utils/DialogUtils.java
@@ -1,14 +1,23 @@
 package com.money.manager.ex.utils;
 
 import android.app.ProgressDialog;
+import android.util.Log;
+
+import com.money.manager.ex.core.ExceptionHandler;
 
 /**
  * Common dialog utility functions.
  */
 public class DialogUtils {
     public static void closeProgressDialog(ProgressDialog progressDialog) {
-        progressDialog.hide();
-        progressDialog.dismiss();
+        try {
+            progressDialog.hide();
+            progressDialog.dismiss();
+        } catch (Exception ex) {
+            //ExceptionHandler handler = new ExceptionHandler(getapp)
+//            Log.e("Dialog Utils", )
+            ex.printStackTrace();
+        }
     }
 
 }
