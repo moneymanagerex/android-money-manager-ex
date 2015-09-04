d594c8ef44b202d0a0bf4d977f49499d81b02463
Alen Siljak <alen.siljak@yahoo.com>
2015-09-04 17:43:22 +0200

    adding headers for the edit fields

--- src/com/money/manager/ex/investment/EditInvestmentTransactionActivity.java
+++ src/com/money/manager/ex/investment/EditInvestmentTransactionActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2012-2015 The Android Money Manager Ex Project Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package com.money.manager.ex.investment;
 
 import android.os.Bundle;
@@ -21,7 +38,7 @@ public class EditInvestmentTransactionActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_edit_investment_transaction, menu);
+//        getMenuInflater().inflate(R.menu.menu_edit_investment_transaction, menu);
         return true;
     }
 
@@ -33,9 +50,9 @@ public class EditInvestmentTransactionActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
