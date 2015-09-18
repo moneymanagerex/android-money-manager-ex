a3b44e9bb7933f24814dc5d54dc16b91305772ab
Alen Siljak <alen.siljak@yahoo.com>
2015-09-18 17:00:14 +0200

    adding initial Gulp script and documentation for automation

---
+++ scripts/gulpfile.js
@@ -0,0 +1,18 @@
+/*global gulp */
+
+// include gulp
+var gulp = require('gulp'); 
+var rename = require('gulp-rename');
+
+gulp.task('copy', function(){
+  console.log("Copy is demonstrating the capability to copy the file to the same directory and rename the file in the process. The output is 'copy.md'.")
+  gulp.src('./ReadMe.md')
+    .pipe(rename('copy.md'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', function() {
+  console.log('Please check ReadMe.md file for instructions or run one of the following commands' + 
+  ' to execute a task:');
+  console.log('copy => copy files')
+});
