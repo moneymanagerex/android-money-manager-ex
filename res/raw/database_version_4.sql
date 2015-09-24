b392f3f23e412b6bf602d657681b2b5d6c042b72
Alen Siljak <alen.siljak@yahoo.com>
2015-09-24 21:27:09 +0200

    #412 semi-columns after the commands

--- res/raw/database_version_4.sql
+++ res/raw/database_version_4.sql
@@ -3,11 +3,11 @@ CREATE TABLE ASSETCLASS_V1 (
     ID integer primary key,
     NAME TEXT COLLATE NOCASE NOT NULL,
     ALLOCATION numeric
-)
+);
 
 -- Asset Class / Stock link table
 CREATE TABLE ASSETCLASS_STOCK_V1 (
     ID integer primary key,
     ASSETCLASSID integer UNIQUE,
     STOCKID integer UNIQUE
-)
+);
