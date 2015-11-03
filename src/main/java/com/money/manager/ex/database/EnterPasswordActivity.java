899f97c1917bd0e646666377391a5cd2a5cf0a35
Alen Siljak <alen.siljak@yahoo.com>
2015-11-03 16:09:32 +0100

    #135 password entry form

---
+++ src/main/java/com/money/manager/ex/database/EnterPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.money.manager.ex.database;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.money.manager.ex.R;
+
+public class EnterPasswordActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_password);
+    }
+
+}
