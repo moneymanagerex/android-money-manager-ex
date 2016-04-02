d2f9e0dbc6a2bb6364b2fb8557527debfedbdf90
Alen Siljak <alen.siljak@yahoo.com>
2016-04-02 11:22:27 +0200

    adding description to selenium test classes

--- app/src/test/java/org/moneymanagerex/android/seleniumTests/MainActivityTests.java
+++ app/src/test/java/org/moneymanagerex/android/seleniumTests/MainActivityTests.java
@@ -24,14 +24,19 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import io.selendroid.client.SelendroidDriver;
+import io.selendroid.client.SelendroidKeys;
 import io.selendroid.common.SelendroidCapabilities;
 import io.selendroid.standalone.SelendroidConfiguration;
 import io.selendroid.standalone.SelendroidLauncher;
 
 /**
- * First Selendroid test
+ * First Selendroid test, proof of concept.
+ * This class works with a manually-run test server. That might be a preferable option when
+ * running a set of tests instead of instantiating the server for each test class.
+ * Run the server from the scripts directory.
  */
 public class MainActivityTests {
     private static WebDriver driver = null;
@@ -54,6 +59,8 @@ public class MainActivityTests {
 
     @Test
     public void runActivity() {
+        driver.findElement(By.id("skipTextView")).click();
+        new Actions(driver).sendKeys(SelendroidKeys.MENU).perform();
 
     }
 }
