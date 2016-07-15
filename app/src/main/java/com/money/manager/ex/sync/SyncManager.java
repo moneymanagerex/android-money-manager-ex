a70a893daf32c6d26f98cbdd20af299bced408e4
Alen Siljak <alen.siljak@yahoo.com>
2016-07-15 13:24:06 +0200

    #879 ignoring the cancelled authentication exceptions

--- app/src/main/java/com/money/manager/ex/sync/SyncManager.java
+++ app/src/main/java/com/money/manager/ex/sync/SyncManager.java
@@ -27,6 +27,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.cloudrail.si.exceptions.AuthenticationException;
 import com.cloudrail.si.exceptions.ParseException;
 import com.cloudrail.si.interfaces.CloudStorage;
 import com.cloudrail.si.services.Box;
@@ -328,6 +329,13 @@ public class SyncManager {
             public void run() {
                 try {
                     getProvider().login();
+                } catch (AuthenticationException e) {
+                    if (e.getMessage().equals("Authentication was cancelled")) {
+                        ExceptionHandler.warn("authentication cancelled");
+                    } else {
+                        ExceptionHandler handler = new ExceptionHandler(getContext());
+                        handler.handle(e, "logging in to cloud provider");
+                    }
                 } catch (Exception e) {
                     ExceptionHandler handler = new ExceptionHandler(getContext());
                     handler.handle(e, "logging in to cloud provider");
