b60027bb8a2f679b8225b39618da270715fc3927
Alen Siljak <alen.siljak@yahoo.com>
2016-08-19 19:01:50 +0200

    minor cleanup

--- app/src/main/java/com/money/manager/ex/sync/SyncPreferenceFragment.java
+++ app/src/main/java/com/money/manager/ex/sync/SyncPreferenceFragment.java
@@ -26,9 +26,6 @@ import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-//import android.support.v7.preference.Preference;
-//import android.support.v7.preference.PreferenceFragmentCompat;
-//import android.support.v7.preference.PreferenceManager;
 
 import android.widget.Toast;
 
