a71c23efed6d9009654c9c91c2b90a0d8bd11925
Velmurugan Cithaiyan <29807533+velmuruganc@users.noreply.github.com>
2018-01-10 06:20:43 +0530

    Minor Correction in isTransactionSms method
    Minor Correction in isTransactionSms method

--- app/src/main/java/com/money/manager/ex/notifications/SmsReceiverTransactions.java
+++ app/src/main/java/com/money/manager/ex/notifications/SmsReceiverTransactions.java
@@ -459,7 +459,7 @@ public class SmsReceiverTransactions extends BroadcastReceiver {
 
         try
         {
-            Pattern p = Pattern.compile("(-[A-Z]+)");
+            Pattern p = Pattern.compile("(-[a-zA-Z]+)");
             Matcher m = p.matcher(smsSender);
 
             if (m != null)
