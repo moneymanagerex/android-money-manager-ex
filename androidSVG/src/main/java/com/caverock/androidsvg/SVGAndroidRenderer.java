24777c727f16152c156efde28dd4c7e49df931db
Alen Siljak <alen.siljak@yahoo.com>
2019-02-09 20:04:22 +0100

    updating androidSVG library

--- androidSVG/src/main/java/com/caverock/androidsvg/SVGAndroidRenderer.java
+++ androidSVG/src/main/java/com/caverock/androidsvg/SVGAndroidRenderer.java
@@ -17,7 +17,6 @@
 package com.caverock.androidsvg;
 
 
-import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -751,6 +750,16 @@ class SVGAndroidRenderer
 
    private void  popLayer(SvgElement obj)
    {
+      popLayer(obj, obj.boundingBox);
+   }
+
+
+   /**
+    * @param obj The object we are compositing. Compositing happens if the obj is not fully opaque, or if it has a mask.
+    * @param originalObjBBox Normally equal to obj.boundingBox. However, if obj is a mask, then this is the bounding box of the original object to which the mask was applied.
+    */
+   private void  popLayer(SvgElement obj, Box originalObjBBox)
+   {
       // If this is masked content, apply the mask now
       if (state.style.mask != null) {
          // The masked content has been drawn, now we have to composite it with our mask layer.
@@ -773,10 +782,11 @@ class SVGAndroidRenderer
            maskPaint1.setColorFilter(new ColorMatrixColorFilter(luminanceToAlpha));
            canvas.saveLayer(null, maskPaint1, Canvas.ALL_SAVE_FLAG);   // TODO use real mask bounds
 
-             // Render the mask content into the step 1 part
+             // Render the mask content into the step 1 layer
              SVG.SvgObject  ref = document.resolveIRI(state.style.mask);
-             renderMask((SVG.Mask) ref, obj);
+             renderMask((SVG.Mask) ref, obj, originalObjBBox);
 
+           // The restore applies the luminanceToAlpha conversion
            canvas.restore();
 
            // Step 2
@@ -785,10 +795,12 @@ class SVGAndroidRenderer
            canvas.saveLayer(null, maskPaint2, Canvas.ALL_SAVE_FLAG);
 
              // Render the mask content (again) into the step 2 part
-             renderMask((SVG.Mask) ref, obj);
+             renderMask((SVG.Mask) ref, obj, originalObjBBox);
 
+           // The retore composites the luminanceToAlpha layer with the masks alpha
            canvas.restore();
 
+         // Apply the final mask to the original object waiting in the open layer created in pushLayer()
          canvas.restore();
       }
 
@@ -2192,6 +2204,7 @@ class SVGAndroidRenderer
 
       if (isSpecified(style, SVG.SPECIFIED_STROKE_MITERLIMIT))
       {
+         // FIXME: must be >= 0
          state.style.strokeMiterLimit = style.strokeMiterLimit;
          state.strokePaint.setStrokeMiter(style.strokeMiterLimit);
       }
@@ -2445,7 +2458,7 @@ class SVGAndroidRenderer
    }
 
 
-   static int  colourWithOpacity(int colour, float opacity)
+   private static int  colourWithOpacity(int colour, float opacity)
    {
       int  alpha = (colour >> 24) & 0xff;
       alpha = Math.round(alpha * opacity);
@@ -2576,15 +2589,15 @@ class SVGAndroidRenderer
    // Handling of Arcs
 
    /*
-    * SVG arc representation uses "endpoint parameterisation" where we specify the endpoint of the arc.
+    * SVG arc representation uses "endpoint parameterisation" where we specify the start and endpoint of the arc.
     * This is to be consistent with the other path commands.  However we need to convert this to "centre point
     * parameterisation" in order to calculate the arc. Handily, the SVG spec provides all the required maths
     * in section "F.6 Elliptical arc implementation notes".
     * 
     * Some of this code has been borrowed from the Batik library (Apache-2 license).
     *
-    * Note: the original version of this code used doubles. This version uses floats because of some
-    * sort Android JIT(?) bug. See Issue #62.
+    * Previously, to work around issue #62, we converted this function to use floats. However in issue #155,
+    * we discovered that there are some arcs that fail due of a lack of precision. So we have switched back to doubles.
     */
 
    private static void arcTo(float lastX, float lastY, float rx, float ry, float angle, boolean largeArcFlag, boolean sweepFlag, float x, float y, PathInterface pather)
@@ -2607,77 +2620,85 @@ class SVGAndroidRenderer
       ry = Math.abs(ry);
 
       // Convert angle from degrees to radians
-      float angleRad = (float) Math.toRadians(angle % 360.0);
-      float cosAngle = (float) Math.cos(angleRad);
-      float sinAngle = (float) Math.sin(angleRad);
-      
+      double angleRad = Math.toRadians(angle % 360.0);
+      double cosAngle = Math.cos(angleRad);
+      double sinAngle = Math.sin(angleRad);
+
       // We simplify the calculations by transforming the arc so that the origin is at the
       // midpoint calculated above followed by a rotation to line up the coordinate axes
       // with the axes of the ellipse.
 
       // Compute the midpoint of the line between the current and the end point
-      float dx2 = (lastX - x) / 2.0f;
-      float dy2 = (lastY - y) / 2.0f;
+      double dx2 = (lastX - x) / 2.0;
+      double dy2 = (lastY - y) / 2.0;
 
-      // Step 1 : Compute (x1', y1') - the transformed start point
-      float x1 = (cosAngle * dx2 + sinAngle * dy2);
-      float y1 = (-sinAngle * dx2 + cosAngle * dy2);
+      // Step 1 : Compute (x1', y1')
+      // x1,y1 is the midpoint vector rotated to take the arc's angle out of consideration
+      double x1 = (cosAngle * dx2 + sinAngle * dy2);
+      double y1 = (-sinAngle * dx2 + cosAngle * dy2);
 
-      float rx_sq = rx * rx;
-      float ry_sq = ry * ry;
-      float x1_sq = x1 * x1;
-      float y1_sq = y1 * y1;
+      double rx_sq = rx * rx;
+      double ry_sq = ry * ry;
+      double x1_sq = x1 * x1;
+      double y1_sq = y1 * y1;
 
       // Check that radii are large enough.
       // If they are not, the spec says to scale them up so they are.
       // This is to compensate for potential rounding errors/differences between SVG implementations.
-      float radiiCheck = x1_sq / rx_sq + y1_sq / ry_sq;
-      if (radiiCheck > 1) {
-         rx = (float) Math.sqrt(radiiCheck) * rx;
-         ry = (float) Math.sqrt(radiiCheck) * ry;
+      double radiiCheck = x1_sq / rx_sq + y1_sq / ry_sq;
+      if (radiiCheck > 0.99999) {
+         double radiiScale = Math.sqrt(radiiCheck) * 1.00001;
+         rx = (float) (radiiScale * rx);
+         ry = (float) (radiiScale * ry);
          rx_sq = rx * rx;
          ry_sq = ry * ry;
       }
 
       // Step 2 : Compute (cx1, cy1) - the transformed centre point
-      float sign = (largeArcFlag == sweepFlag) ? -1 : 1;
-      float sq = ((rx_sq * ry_sq) - (rx_sq * y1_sq) - (ry_sq * x1_sq)) / ((rx_sq * y1_sq) + (ry_sq * x1_sq));
+      double sign = (largeArcFlag == sweepFlag) ? -1 : 1;
+      double sq = ((rx_sq * ry_sq) - (rx_sq * y1_sq) - (ry_sq * x1_sq)) / ((rx_sq * y1_sq) + (ry_sq * x1_sq));
       sq = (sq < 0) ? 0 : sq;
-      float coef = (float) (sign * Math.sqrt(sq));
-      float cx1 = coef * ((rx * y1) / ry);
-      float cy1 = coef * -((ry * x1) / rx);
+      double coef = (sign * Math.sqrt(sq));
+      double cx1 = coef * ((rx * y1) / ry);
+      double cy1 = coef * -((ry * x1) / rx);
 
       // Step 3 : Compute (cx, cy) from (cx1, cy1)
-      float sx2 = (lastX + x) / 2.0f;
-      float sy2 = (lastY + y) / 2.0f;
-      float cx = sx2 + (cosAngle * cx1 - sinAngle * cy1);
-      float cy = sy2 + (sinAngle * cx1 + cosAngle * cy1);
+      double sx2 = (lastX + x) / 2.0;
+      double sy2 = (lastY + y) / 2.0;
+      double cx = sx2 + (cosAngle * cx1 - sinAngle * cy1);
+      double cy = sy2 + (sinAngle * cx1 + cosAngle * cy1);
 
       // Step 4 : Compute the angleStart (angle1) and the angleExtent (dangle)
-      float ux = (x1 - cx1) / rx;
-      float uy = (y1 - cy1) / ry;
-      float vx = (-x1 - cx1) / rx;
-      float vy = (-y1 - cy1) / ry;
-      float p, n;
+      double ux = (x1 - cx1) / rx;
+      double uy = (y1 - cy1) / ry;
+      double vx = (-x1 - cx1) / rx;
+      double vy = (-y1 - cy1) / ry;
+      double p, n;
 
-      // Compute the angle start
-      n = (float) Math.sqrt((ux * ux) + (uy * uy));
-      p = ux; // (1 * ux) + (0 * uy)
-      sign = (uy < 0) ? -1.0f : 1.0f;
-      float angleStart = (float) Math.toDegrees(sign * Math.acos(p / n));
+      // Angle betwen two vectors is +/- acos( u.v / len(u) * len(v))
+      // Where '.' is the dot product. And +/- is calculated from the sign of the cross product (u x v)
+
+      final double  TWO_PI = Math.PI * 2.0;
+
+      // Compute the start angle
+      // The angle between (ux,uy) and the 0deg angle (1,0)
+      n = Math.sqrt((ux * ux) + (uy * uy));  // len(u) * len(1,0) == len(u)
+      p = ux;                                // u.v == (ux,uy).(1,0) == (1 * ux) + (0 * uy) == ux
+      sign = (uy < 0) ? -1.0 : 1.0;          // u x v == (1 * uy - ux * 0) == uy
+      double angleStart = sign * Math.acos(p / n);  // No need for checkedArcCos() here
 
       // Compute the angle extent
-      n = (float) Math.sqrt((ux * ux + uy * uy) * (vx * vx + vy * vy));
+      n = Math.sqrt((ux * ux + uy * uy) * (vx * vx + vy * vy));
       p = ux * vx + uy * vy;
       sign = (ux * vy - uy * vx < 0) ? -1.0f : 1.0f;
-      double angleExtent = Math.toDegrees(sign * Math.acos(p / n));
+      double angleExtent = sign * checkedArcCos(p / n);
       if (!sweepFlag && angleExtent > 0) {
-         angleExtent -= 360f;
+         angleExtent -= TWO_PI;
       } else if (sweepFlag && angleExtent < 0) {
-         angleExtent += 360f;
+         angleExtent += TWO_PI;
       }
-      angleExtent %= 360f;
-      angleStart %= 360f;
+      angleExtent %= TWO_PI;
+      angleStart %= TWO_PI;
 
       // Many elliptical arc implementations including the Java2D and Android ones, only
       // support arcs that are axis aligned.  Therefore we need to substitute the arc
@@ -2689,7 +2710,7 @@ class SVGAndroidRenderer
       Matrix m = new Matrix();
       m.postScale(rx, ry);
       m.postRotate(angle);
-      m.postTranslate(cx, cy);
+      m.postTranslate((float) cx, (float) cy);
       m.mapPoints(bezierPoints);
 
       // The last point in the bezier set should match exactly the last coord pair in the arc (ie: x,y). But
@@ -2704,6 +2725,14 @@ class SVGAndroidRenderer
       {
          pather.cubicTo(bezierPoints[i], bezierPoints[i+1], bezierPoints[i+2], bezierPoints[i+3], bezierPoints[i+4], bezierPoints[i+5]);
       }
+   }
+
+
+   // Check input to Math.acos() in case rounding or other errors result in a val < -1 or > +1.
+   // For example, see the possible KitKat JIT error described in issue #62.
+   private static double  checkedArcCos(double val)
+   {
+      return (val < -1.0) ? Math.PI : (val > 1.0) ? 0 : Math.acos(val);
    }
 
 
@@ -2723,11 +2752,9 @@ class SVGAndroidRenderer
     */
    private static float[]  arcToBeziers(double angleStart, double angleExtent)
    {
-      int    numSegments = (int) Math.ceil(Math.abs(angleExtent) / 90.0);
-      
-      angleStart = Math.toRadians(angleStart);
-      angleExtent = Math.toRadians(angleExtent);
-      float  angleIncrement = (float) (angleExtent / numSegments);
+      int    numSegments = (int) Math.ceil(Math.abs(angleExtent) * 2.0 / Math.PI);  // (angleExtent / 90deg)
+
+      double  angleIncrement = angleExtent / numSegments;
       
       // The length of each control point vector is given by the following formula.
       double  controlLength = 4.0 / 3.0 * Math.sin(angleIncrement / 2.0) / (1.0 + Math.cos(angleIncrement / 2.0));
@@ -3809,7 +3836,7 @@ class SVGAndroidRenderer
          path.setFillType(getClipRuleFromState());
       }
       else {
-         error("Invalid %s element found in clipPath definition", obj.getClass().getSimpleName());
+         error("Invalid %s element found in clipPath definition", obj.getNodeName());
          return null;
       }
 
@@ -3856,7 +3883,7 @@ class SVGAndroidRenderer
       boolean  userUnits = (clipPath.clipPathUnitsAreUser == null || clipPath.clipPathUnitsAreUser);
 
       if ((obj instanceof SVG.Group) && !userUnits) {
-         warn("<clipPath clipPathUnits=\"objectBoundingBox\"> is not supported when referenced from container elements (like %s)", obj.getClass().getSimpleName());
+         warn("<clipPath clipPathUnits=\"objectBoundingBox\"> is not supported when referenced from container elements (like %s)", obj.getNodeName());
          return;
       }
 
@@ -3912,7 +3939,7 @@ class SVGAndroidRenderer
       } else if (obj instanceof SVG.GraphicsElement) {
          addObjectToClip((SVG.GraphicsElement) obj, combinedPath, combinedPathMatrix);
       } else {
-         error("Invalid %s element found in clipPath definition", obj.getClass().getSimpleName());
+         error("Invalid %s element found in clipPath definition", obj.toString());
       }
 
       // Restore state
@@ -3923,11 +3950,10 @@ class SVGAndroidRenderer
    // The clip state push and pop methods only save the matrix.
    // The normal push/pop save the clip region also which would
    // destroy the clip region we are trying to build.
-   @SuppressLint("WrongConstant")  // MATRIX_SAVE_FLAG is deprecated and being flagged as an error by Android Studio
    private void  clipStatePush()
    {
-      // Save matrix and clip
-      canvas.save(Canvas.MATRIX_SAVE_FLAG);
+      // Save matrix but not clip
+      CanvasLegacy.save(canvas, CanvasLegacy.MATRIX_SAVE_FLAG);
       // Save style state
       stateStack.push(state);
       state = new RendererState(state);
@@ -4514,7 +4540,7 @@ class SVGAndroidRenderer
    /*
     * Render the contents of a mask element.
     */
-   private void  renderMask(SVG.Mask mask, SvgElement obj)
+   private void  renderMask(SVG.Mask mask, SvgElement obj, Box originalObjBBox)
    {
       debug("Mask render");
 
@@ -4523,8 +4549,8 @@ class SVGAndroidRenderer
 
       if (maskUnitsAreUser)
       {
-         w = (mask.width != null) ? mask.width.floatValueX(this): obj.boundingBox.width;
-         h = (mask.height != null) ? mask.height.floatValueY(this): obj.boundingBox.height;
+         w = (mask.width != null) ? mask.width.floatValueX(this): originalObjBBox.width;
+         h = (mask.height != null) ? mask.height.floatValueY(this): originalObjBBox.height;
          //x = (mask.x != null) ? mask.x.floatValueX(this): (float)(obj.boundingBox.minX - 0.1 * obj.boundingBox.width);
          //y = (mask.y != null) ? mask.y.floatValueY(this): (float)(obj.boundingBox.minY - 0.1 * obj.boundingBox.height);
       }
@@ -4535,10 +4561,10 @@ class SVGAndroidRenderer
          //y = (mask.y != null) ? mask.y.floatValue(this, 1f): -0.1f;
          w = (mask.width != null) ? mask.width.floatValue(this, 1f): 1.2f;
          h = (mask.height != null) ? mask.height.floatValue(this, 1f): 1.2f;
-         //x = obj.boundingBox.minX + x * obj.boundingBox.width;
-         //y = obj.boundingBox.minY + y * obj.boundingBox.height;
-         w *= obj.boundingBox.width;
-         h *= obj.boundingBox.height;
+         //x = originalObjBBox.minX + x * originalObjBBox.width;
+         //y = originalObjBBox.minY + y * originalObjBBox.height;
+         w *= originalObjBBox.width;
+         h *= originalObjBBox.height;
       }
       if (w == 0 || h == 0)
          return;
@@ -4549,18 +4575,28 @@ class SVGAndroidRenderer
       state = findInheritFromAncestorState(mask);
       // Set the style for the mask (inherits from its own ancestors, not from callee's state)
       // The 'opacity', 'filter' and 'display' properties do not apply to the 'mask' element" (sect 14.4)
-      // Next line is not actually needed since we aren't calling pushLayer() here. Kept for future reference.
       state.style.opacity = 1f;
       //state.style.filter = null;
 
+      boolean  compositing = pushLayer();
+
+      // Save the current transform matrix, as we need to undo the following transform straight away
+      canvas.save();
+
       boolean  maskContentUnitsAreUser = (mask.maskContentUnitsAreUser == null || mask.maskContentUnitsAreUser);
       if (!maskContentUnitsAreUser) {
-         canvas.translate(obj.boundingBox.minX, obj.boundingBox.minY);
-         canvas.scale(obj.boundingBox.width, obj.boundingBox.height);
+         canvas.translate(originalObjBBox.minX, originalObjBBox.minY);
+         canvas.scale(originalObjBBox.width, originalObjBBox.height);
       }
 
       // Render the mask
       renderChildren(mask, false);
+
+      // Restore the matrix so that, if this mask has a mask, it is not affected by the objectBoundingBox transform
+      canvas.restore();
+
+      if (compositing)
+         popLayer(obj, originalObjBBox);
 
       // Pop the state
       statePop();
