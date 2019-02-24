9bc4ae72d52f79fc0442b2ea0f301ce9b72648b4
funrider1 <enrico.kucher@gmail.com>
2019-02-24 21:47:02 +0100

    update copyright to 2019

--- app/src/main/java/com/money/manager/ex/widget/SingleAccountWidgetConfigureActivity.java
+++ app/src/main/java/com/money/manager/ex/widget/SingleAccountWidgetConfigureActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2018 The Android Money Manager Ex Project Team
+ * Copyright (C) 2012-2019 The Android Money Manager Ex Project Team
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
