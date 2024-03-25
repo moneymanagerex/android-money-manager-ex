941cd1c36fa38ae52b7ed3a6a9f7b1b93b5e7362
Lisheng Guan <guanlisheng@gmail.com>
2024-03-26 07:05:35 +0800

    refactor update with args

--- app/src/main/java/com/money/manager/ex/common/CategoryListFragment.java
+++ app/src/main/java/com/money/manager/ex/common/CategoryListFragment.java
@@ -638,7 +638,7 @@ public class CategoryListFragment
                                 if (getActivity().getContentResolver().update(
                                         repo.getUri(),
                                         values,
-                                        Category.CATEGID + "=" + categoryId, null) == 0) {
+                                        Category.CATEGID + "=?", new String[]{Integer.toString(categoryId)}) == 0) {
                                     Toast.makeText(getActivity(), R.string.db_update_failed, Toast.LENGTH_SHORT).show();
                                 }
                                 break;
