1102cc78c931ced3c0eeb260947e06d7ac169067
Lisheng Guan <guanlisheng@gmail.com>
2024-11-12 15:17:41 +0800

    UUID: int2long clean up

--- app/src/main/java/com/money/manager/ex/budget/BudgetsActivity.java
+++ app/src/main/java/com/money/manager/ex/budget/BudgetsActivity.java
@@ -67,7 +67,7 @@ public class BudgetsActivity
         // Handle action bar item clicks here. The action bar will
         // automatically e clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        long id = item.getItemId();
+        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
