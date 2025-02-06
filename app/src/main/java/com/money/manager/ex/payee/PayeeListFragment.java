ca8daeed90fbe4d5d6aea85388472cc73becdfaa
Emmanuele (WolfSolver) <wolfsolver@prudenzano.org>
2025-02-06 16:20:17 +0100

    Backward compatibility for payee active = null close #2216

--- app/src/main/java/com/money/manager/ex/payee/PayeeListFragment.java
+++ app/src/main/java/com/money/manager/ex/payee/PayeeListFragment.java
@@ -115,7 +115,13 @@ public class PayeeListFragment
         adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
             public boolean setViewValue(View aView, Cursor aCursor, int aColumnIndex) {
                 TextView textView = (TextView) aView;
-                boolean active = ( Integer.parseInt(aCursor.getString(aCursor.getColumnIndex(Payee.ACTIVE))) != 0);
+                boolean active;
+                if (aCursor.getString(aCursor.getColumnIndex(Payee.ACTIVE))==null) {
+                    // issue 2216: consider true if active column is not set, backward compatibulity
+                    active = true;
+                } else {
+                    active = (Integer.parseInt(aCursor.getString(aCursor.getColumnIndex(Payee.ACTIVE))) != 0);
+                }
                 CharSequence text = aCursor.getString(aColumnIndex);
                 if (!TextUtils.isEmpty(adapter.getHighlightFilter())) {
                     text = adapter.getCore().highlight(adapter.getHighlightFilter(),text.toString());
