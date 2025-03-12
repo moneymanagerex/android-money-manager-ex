c49c2e4dfff4c55ab912aee0f26611dcb779eb3b
Emmanuele (WolfSolver) <wolfsolver@prudenzano.org>
2025-03-12 16:59:13 +0100

    CrashReport Refinement

--- app/src/main/java/com/money/manager/ex/crashreport/CrashReportActivity.java
+++ app/src/main/java/com/money/manager/ex/crashreport/CrashReportActivity.java
@@ -10,7 +10,10 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.money.manager.ex.Constants;
 import com.money.manager.ex.R;
+
+import timber.log.Timber;
 
 public class CrashReportActivity extends AppCompatActivity {
     private Intent intent = null;
@@ -35,26 +38,53 @@ public class CrashReportActivity extends AppCompatActivity {
         String report = intent.getStringExtra(Intent.EXTRA_TEXT);
 
         TextView reportUI = findViewById(R.id.editTextReport);
-        reportUI.setText(report);
+        if (reportUI != null)
+            reportUI.setText(report);
+
+        Button sendMail = findViewById(R.id.buttonMail);
+        if (sendMail != null) {
+            sendMail.setOnClickListener(v -> {
+                // send mail
+                //send file using email
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{Constants.EMAIL});
+                emailIntent.putExtra(Intent.EXTRA_TEXT, report);
+                // the mail subject
+                emailIntent .putExtra(Intent.EXTRA_SUBJECT, "Android Crash Report:");
+
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                } catch (Exception e) {
+//                    Timber.e(e, "opening email with logcat");
+                }
+
+            });
+        }
+
 
         Button openIssue = findViewById(R.id.buttonOpenIssue);
-        openIssue.setOnClickListener(v -> {
-            String body = "[Put here your description]\n" + report;
-            String uri = Uri.parse("https://github.com/moneymanagerex/android-money-manager-ex/issues/new")
-                    .buildUpon()
-                    .appendQueryParameter("label", "bug")
+        if ( openIssue != null ) {
+            openIssue.setOnClickListener(v -> {
+                String body = "[Put here your description]\n" + report;
+                String uri = Uri.parse("https://github.com/moneymanagerex/android-money-manager-ex/issues/new")
+                        .buildUpon()
+                        .appendQueryParameter("label", "bug")
 //                .appendQueryParameter("title", "Your title here")
-                    .appendQueryParameter("body", body)
-                    .build().toString();
-            Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-            this.startActivity(myIntent);
+                        .appendQueryParameter("body", body)
+                        .build().toString();
+                Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                this.startActivity(myIntent);
 
-            closeActivity();
+                closeActivity();
 
-        });
+            });
+        }
 
         Button cancel = findViewById(R.id.buttonQuit);
-        cancel.setOnClickListener(view -> closeActivity());
+        if ( cancel != null ) {
+            cancel.setOnClickListener(view -> closeActivity());
+        }
     }
 
     private void closeActivity() {
